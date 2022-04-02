5f4496c0e0d2594a901010b97c6ca351cbad3786
adnan <adnanbezerra@gmail.com>
2022-04-02 16:15:22 -0300

    sabadou

--- sistema.js
+++ sistema.js
@@ -1,14 +1,78 @@
 let numeroPedidos = 0;
 let valorPedidos = 0;
 
-function selecionarPrato() {
-
+// Função que coordena o novo pedido
+function selecionarPrato(pedido, tipo) {
+    selecionar(pedido, tipo)
 }
 
-function selecionarBebida() {
-
+function selecionarBebida(pedido, tipo) {
+    selecionar(pedido, tipo)
 }
 
-function selecionarSobremesa() {
+function selecionarSobremesa(pedido, tipo) {
+    selecionar(pedido, tipo)
+}
+
+function selecionar(pedido, tipo) {
+
+    let consulta = `.${tipo}` + ".pedido-selecionado";
     
+    let pedidoAnterior = document.querySelector(consulta);
+
+    if (pedido === pedidoAnterior) {
+
+        console.log("ódio")
+        pedido.classList.toggle("pedido-selecionado");
+        const valor = pedido.querySelector(".valor")
+        let valorHTML = Number(valor.innerText.replace(",", "."))
+
+        numeroPedidos -= 1;
+
+        valorPedidos -= valorHTML;
+
+        return;
+    }
+
+    if (pedidoAnterior) {
+
+        console.log("desgraça")
+        const pedidoAnterior = document.querySelector(consulta);
+        pedidoAnterior.classList.remove("pedido-selecionado");
+
+        let valorAntigo = pedidoAnterior.querySelector(".valor")
+        let valorAntigoHTML = Number(valorAntigo.innerText.replace(",", "."));
+        valorPedidos -= valorAntigoHTML;
+
+        let valor = pedido.querySelector(".valor");
+        let valorHTML = Number(valor.innerText.replace(",", "."));
+        pedido.classList.toggle("pedido-selecionado");
+
+        valorPedidos += valorHTML;
+
+    } else {
+        console.log("pinoia")
+
+        const valor = pedido.querySelector(".valor")
+        let valorHTML = Number(valor.innerText.replace(",", "."))
+        pedido.classList.toggle("pedido-selecionado");
+
+        numeroPedidos++;
+        valorPedidos += valorHTML;
+    }
+
+    console.log(valorPedidos)
+    atualizarBotao()
+}
+
+function atualizarBotao() {
+    const botao = document.querySelector(".pedir");
+
+    if (numeroPedidos === 3) {
+        botao.classList.add("botao-selecionado")
+        botao.innerHTML = "Pedir";
+        botao.disabled = false;
+    } else {
+        botao.disabled = true;
+    }
 }
